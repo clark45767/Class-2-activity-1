78fe80c6a1dc6979ef5688af3e89cd534ba388de
clark45767 <clarkigarma@gmail.com>
2026-02-12 17:58:00 +0400

    Add files via upload

---
+++ activity.py
@@ -0,0 +1,10 @@
+name = "Clark"
+x = 5
+is_Teacher = False
+y= 6.7
+print("Data type of name is : ", type(name))
+print ("Data type of x is :", type(x))
+print ("Data type of y is :", type(y))
+print("Data type of is_teacher is :", type( is_Teacher))
+x = float(x)
+print ("Data type of x is :", type(x))
